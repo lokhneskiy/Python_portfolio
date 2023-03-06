472b10e57871bb5c826cf6766a0836b77c76edeb
lokhneskiy <lokhneskiy@gmail.com>
2023-03-06 17:59:25 +0200

    created elements_test / Textbox added elements_page.py, elements_page_locators.py  elements_test.py , generator.py , data.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -13,13 +13,13 @@ class BasePage:
     def element_is_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_element_located(locator))
 
-    def element_are_visible(self, locator, timeout=5):
+    def elements_are_visible(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.visibility_of_all_elements_located(locator))
 
     def element_is_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_element_located(locator))
 
-    def element_are_present(self, locator, timeout=5):
+    def elements_are_present(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.presence_of_all_elements_located(locator))
 
     def element_is_not_visible(self, locator, timeout=5):
@@ -28,6 +28,5 @@ class BasePage:
     def element_is_clickable(self, locator, timeout=5):
         return wait(self.driver, timeout).until(EC.element_to_be_clickable(locator))
 
-    def go_t0_element(self, element):
-        self.driver.exrcute_script("argument[0].scrollIntoView();", element)
-
+    def go_to_element(self, element):
+        self.driver.execute_script("arguments[0].scrollIntoView();", element)
